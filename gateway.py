5e21bdd45d09e78aecee4ef7ac9557f8c7381354
eric-r-g <eric.rodrig.gomes@gmail.com>
2025-06-28 13:25:11 -0300

    Update gateway.py
    adicionado função server_cliente para estabelecer uma comunicação com o cliente, além de adicionado comentários ao código.

--- gateway.py
+++ gateway.py
@@ -1,42 +1,80 @@
 import socket
 import serjipe_message_pb2
-
-MULTICAST_GROUP = '239.1.2.3'
-PORT = 5000
-
-# cria um socket multcast
+from threading import Thread
 
 devices = []
 
-def multcast():
-    # entra no socket multcast
-    socket_multcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    socket_multcast.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_multcast.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1) 
+PORT_CLIENTE = 8000
 
-    socket_multcast.bind(('0.0.0.0', PORT))
+MULTICAST_GROUP = '239.1.2.3'
+PORT_MULTICAST = 5000
+
+
+# cria um socket com o cliente
+def server_cliente():
+    # cria um socket tcp
+    socker_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # liga o socket
+    socker_cliente.bind(('0.0.0.0', PORT_CLIENTE))
+
+    # espera o cliente se conectar
+    socker_cliente.listen(1)
+    conn, addr = socker_cliente.accept()
+    print(f"conexão realizada com {addr[0]}:{addr[1]}")
+
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            # aqui realizar as respostas devidas pra cada uma das requisições
+            conn.sendall(b"ECHO: " + data) # retornar a resposta devida (ainda falta)
+    except Exception as e:
+        print(f"erro: {e}")
+    finally:
+        # encerra as conexões
+        conn.close()
+        socker_cliente.close()
+    # termina o processo
+
+
+
+# realiza um multicast para verificar dispositivos válidos
+# adicionar uma memoria cache e talvez um processe de fazer a chamada multicast de tempos em tempos
+def multicast():
+    # cria um socket udp
+    socket_multicast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    # configura o socket criado
+    socket_multicast.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)      # permite o reuso
+    socket_multicast.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)  # tempo de vida
+
+    # liga o socket 
+    socket_multicast.bind(('0.0.0.0', PORT_MULTICAST))
 
     mreq = socket.inet_aton(MULTICAST_GROUP) + socket.inet_aton('0.0.0.0')
-    socket_multcast.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    socket_multicast.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 
 
-    # cria o comando para chamar os MULTCAST
+    # cria a mensagem para chamar os MULTICAST
     command = serjipe_message_pb2.Command()
-    # o campo parameter se tornou inutil no multicast, porém para sistemas mais avançados se torna importante
+    # o campo parameter se tornou inutil no multicast, porém para sistemas mais avançados pode ser usado
     command.device_id = "gateway-1"
     command.action = "MULTICAST"
     command_bytes = command.SerializeToString()
 
     # envia as requisições para todos os dispositivos
-    socket_multcast.sendto(command_bytes, (MULTICAST_GROUP, PORT))
+    socket_multicast.sendto(command_bytes, (MULTICAST_GROUP, PORT_MULTICAST))
 
+    # talvez alterar essa parte pra não voltar no socket multicast, e sim um socket udp
     # vai escutar as respostas, esperando durante 10 segundos
-    socket_multcast.settimeout(10.0)
+    socket_multicast.settimeout(10.0)
     try:
         while(True):
             # recebe os dados com os d
-            data, addr = socket_multcast.recvfrom(1024)
+            data, addr = socket_multicast.recvfrom(1024)
             response = serjipe_message_pb2.DeviceInfo()
             response.ParseFromString(data)
             devices.append(response)
@@ -48,8 +86,6 @@ def multcast():
 
     finally:
         # fecha a socket
-        socket_multcast.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
-        socket_multcast.close()
-
-    # falta agora só escutar
+        socket_multicast.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+        socket_multicast.close()
 
