29e7c0a97f4eda35768693548ba8480055b42542
Eric <eric.rodrig.gomes@gmail.com>
2025-07-16 20:27:54 -0300

    oxi

--- client/backend/lib/handlerComando.js
+++ client/backend/lib/handlerComando.js
@@ -11,25 +11,11 @@ const packageDefinition = protoLoader.loadSync("serjipe_message.proto",{
 
 const serjipe = grpc.loadPackageDefinition(packageDefinition);
 
-function callback(err, retorno){
-    if (err) {
-        // tratamento de erro devido
-    }
-    else {
-        // atualiza com o deviceInfo devido
-    }
-}
-
-function handleComando(endereco, acao, parametro, callback){
+export function handleComando(endereco, command, callback){
     const client = new serjipe.ControleDispositivosService(
         endereco,
         grpc.credentials.createInsecure()
     );
-
-    const command = {
-        action: acao,
-        parameter: parametro
-    }
 
     client.EnviarComando(command, (err, retorno) => {
         client.close();
