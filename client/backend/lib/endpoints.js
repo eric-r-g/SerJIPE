c5df71ff85ad5459713f2d19d4d0a6620f203aab
Eric <eric.rodrig.gomes@gmail.com>
2025-07-16 20:25:17 -0300

    adicionado mais sobre o handleComando

--- client/backend/lib/endpoints.js
+++ client/backend/lib/endpoints.js
@@ -1,4 +1,5 @@
 // Função pra organizar os endpoints num lugar separado e deixar um pouco mais legível
+import { handleComando } from "./handlerComando";
 export function setEndpoints(app){
     app.server.get('/api/dispositivos', (req, res) =>{
         try{
@@ -22,12 +23,23 @@ export function setEndpoints(app){
         const data = req.body;
 
         try{
+            endereco = app.devicesList.getDevice(data.device_id).grpc_endpoint;
             let command = {
-                device_id: data.device_Id,
                 action: data.action,
                 parameter: data.parameter
             }
-            // ToDo: enviar comando com grpc
+
+            handleComando(endereco, command, (err, retorno) => {
+                if (err) {
+                    // tratamento erro
+                }
+                else {
+                    // aqui vai ser adicionado o objeto
+                    app.devicesList.addDevice();
+                }
+                
+            });
+            
         }catch(e){
             res.status(500).send("Erro inesperado ao enviar comando para o gateway: "+err.message);
         }
